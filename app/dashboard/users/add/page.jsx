90c426e836787357bf7515e30d37b48b0b1d65c3
shahid ali <beingshahidali@gmail.com>
2024-09-17 18:20:19 +0530

    Add new user page completed

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,43 @@
+import styles from "@/app/ui/dashboard/users/addUser/addUser.module.css";
+
+const AddUser = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input placeholder="Username" type="text" name="username" required />
+        <input placeholder="Email" type="email" name="email" required />
+        <input
+          placeholder="Password"
+          type="password"
+          name="password"
+          required
+        />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>
+            Is Admin?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true} selected>
+            Is Active?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+
+        <textarea
+          placeholder="Address"
+          name="address"
+          id="address"
+          cols="5"
+          rows="16"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
