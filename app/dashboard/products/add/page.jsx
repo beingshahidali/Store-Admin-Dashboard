60db4f3ac2906000e1f91d223f5e431c7d9eb6c2
shahid ali <beingshahidali@gmail.com>
2024-09-17 18:21:48 +0530

    Add new product page added

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,31 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddProduct = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input placeholder="title" type="text" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input placeholder="Price" type="number" name="price" />
+        <input placeholder="Stock" type="number" name="stock" />
+        <input placeholder="Color" type="text" name="color" />
+        <input placeholder="Size" type="text" name="size" />
+        <textarea
+          placeholder="Description"
+          name="desc"
+          id="desc"
+          cols="5"
+          rows="16"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProduct;
