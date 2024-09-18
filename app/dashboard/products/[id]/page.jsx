7a5cad37f5369db8e77cca5f7aa72690499f5dcb
shahid ali <beingshahidali@gmail.com>
2024-09-18 20:07:20 +0530

    Initial Single product and users page created

---
+++ app/dashboard/products/[id]/page.jsx
@@ -0,0 +1,7 @@
+import styles from "@/app/ui/dashboard/products/singleProductPage/singleProductPage.module.css";
+
+const SingleProductPage = () => {
+  return <div className={styles.container}>SingleProductPage</div>;
+};
+
+export default SingleProductPage;
