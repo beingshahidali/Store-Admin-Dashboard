4029628bd3fd0b5da2560163c8c9e4a6b04e798d
shahid ali <beingshahidali@gmail.com>
2024-09-20 16:11:43 +0530

    Create login UI

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2>Login</h2>
+        <input type="text" placeholder="Username" name="username" />
+        <input type="password" placeholder="Password" name="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
