786eebb16b0c51c0698e8c3a4b5b9012b5730d44
shahid ali <beingshahidali@gmail.com>
2024-09-21 16:20:20 +0530

    Search functionality added

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,10 +1,11 @@
 import { User } from "./models";
 import { connectToDb } from "./utils";
 
-export const fetchUsers = async () => {
+export const fetchUsers = async (q) => {
+  const regEx = new RegExp(q, "i");
   try {
     connectToDb();
-    const users = await User.find();
+    const users = await User.find({ username: { $regex: regEx } });
     return users;
   } catch (e) {
     throw new Error(e);
