05c533936f211f5262e9f271a233057a5f878664
shahid ali <beingshahidali@gmail.com>
2024-09-21 15:30:24 +0530

    Users fetched from DB

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 export const connectToDb = async () => {
   const connection = {};
   try {
-    if (connection.isConnected) return;
+    if (connection.isConnected) {
+      console.log("Already connected");
+      return;
+    }
     const db = await mongoose.connect(
       "mongodb://127.0.0.1:27017/Store_Admin_Dashboard"
     );
-    connection.isConnected = db.connection[0].readyState;
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Connected to MongoDB successfully");
   } catch (e) {
     throw new Error("Couldn't connect to Mongoose" + e.message);
   }
