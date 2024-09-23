0abaa1a3f32f04241c72f2461aeb0e218f5bbca0
shahid ali <beingshahidali@gmail.com>
2024-09-23 17:39:59 +0530

    Added logout functionality

--- app/ui/dashboard/sidebar/Sidebar.jsx
+++ app/ui/dashboard/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import MenuLink from "./menuLink/MenuLink";
+import MenuLink from "./menuLink/menuLink";
 import styles from "./sidebar.module.css";
 import {
   MdDashboard,
@@ -13,98 +13,109 @@ import {
   MdHelpCenter,
   MdLogout,
 } from "react-icons/md";
-const Sidebar = () => {
-  const menuItems = [
-    {
-      title: "Pages",
-      list: [
-        {
-          title: "Dashboard",
-          path: "/dashboard",
-          icon: <MdDashboard />,
-        },
-        {
-          title: "Users",
-          path: "/dashboard/users",
-          icon: <MdSupervisedUserCircle />,
-        },
-        {
-          title: "Products",
-          path: "/dashboard/products",
-          icon: <MdShoppingBag />,
-        },
-        {
-          title: "Transactions",
-          path: "/dashboard/transactions",
-          icon: <MdAttachMoney />,
-        },
-      ],
-    },
-    {
-      title: "Analytics",
-      list: [
-        {
-          title: "Revenue",
-          path: "/dashboard/revenue",
-          icon: <MdWork />,
-        },
-        {
-          title: "Reports",
-          path: "/dashboard/reports",
-          icon: <MdAnalytics />,
-        },
-        {
-          title: "Teams",
-          path: "/dashboard/teams",
-          icon: <MdPeople />,
-        },
-      ],
-    },
-    {
-      title: "User",
-      list: [
-        {
-          title: "Settings",
-          path: "/dashboard/settings",
-          icon: <MdOutlineSettings />,
-        },
-        {
-          title: "Help",
-          path: "/dashboard/help",
-          icon: <MdHelpCenter />,
-        },
-      ],
-    },
-  ];
+import { auth, signOut } from "@/app/auth";
 
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
+const Sidebar = async () => {
+  const { user } = await auth();
   return (
     <div className={styles.container}>
       <div className={styles.user}>
         <Image
-          src="/noAvatar.png"
           className={styles.userImage}
-          alt="Image user"
-          height={50}
-          width={50}
+          src={user.img || "/noavatar.png"}
+          alt=""
+          width="50"
+          height="50"
         />
         <div className={styles.userDetails}>
-          <span className={styles.userName}>John Doe</span>
+          <span className={styles.username}>{user.username}</span>
           <span className={styles.userTitle}>Administrator</span>
         </div>
       </div>
       <ul className={styles.list}>
-        {menuItems.map((menuItem) => (
-          <li key={menuItem?.title}>
-            <span className={styles.cat}>{menuItem?.title}</span>
-            {menuItem?.list?.map((item) => (
-              <MenuLink key={item.title} item={item} />
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
             ))}
           </li>
         ))}
       </ul>
-      <button className={styles.logout}>
-        <MdLogout /> <span>Logout</span>
-      </button>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+      </form>
     </div>
   );
 };
